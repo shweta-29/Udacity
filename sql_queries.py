b9f446aab409df8746b9ab8d1bcdd619c9ae7df4
Shweta Yadav <86231288+shweta-29@users.noreply.github.com>
2022-01-10 23:42:49 +0000

    Add files via upload

--- sql_queries.py
+++ sql_queries.py
@@ -7,48 +7,49 @@ artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
-
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS SONGPLAYS (songplay_id int, start_time timestamp, \
-                            user_id int, level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent text);
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS SONGPLAYS (songplay_id int PRIMARY KEY, start_time timestamp, \
+                            user_id int, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent text);
 """)
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS USERS (user_id int, first_name varchar, \
+user_table_create = ("""CREATE TABLE IF NOT EXISTS USERS (user_id varchar PRIMARY KEY, first_name varchar, \
                         last_name varchar, gender varchar, level varchar);
 """)
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS SONGS (song_id varchar, title text, \
-                        artist_id int, year int, duration float);
+song_table_create = ("""CREATE TABLE IF NOT EXISTS SONGS (song_id varchar PRIMARY KEY, title text, \
+                        artist_id varchar, year int, duration float);
 """)
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id int, name text, \
-                        location text, latitude float, longitude float);
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, artist_name varchar, \
+                        artist_location varchar, artist_latitude float, artist_longitude float);
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time timestamp, hour int, day int, week int, month int, year int, weekday varchar);
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time time PRIMARY KEY, hour int, day int, week int, month int, year int, weekday int);
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, \
+                            user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO users (user_id, first_name, \
+                        last_name, gender, level) VALUES (%s, %s, %s, %s, %s);
 """)
 
-song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (song_data);
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s);
 """)
 
-artist_table_insert = ("""INSERT INTO artists (artist_id, name, \
-                        location, latitude, longitude) VALUES (artist_data);
+artist_table_insert = ("""INSERT INTO artists (artist_id, artist_name, \
+                        artist_location, artist_latitude, artist_longitude) VALUES (%s, %s, %s, %s, %s);
 """)
 
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
-
-song_select = ("""
+#Implement the song_select query in sql_queries.py to find the song ID and artist ID based on the title, artist name, and duration of a song.
+song_select = ("""SELECT s.song_id, a.artist_id FROM songs s, artists a WHERE title = '%s' AND artist name = '%s'AND duration = '%s'
 """)
 
 # QUERY LISTS
